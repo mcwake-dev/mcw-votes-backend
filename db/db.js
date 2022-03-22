f9ab259f94e5c7d50ad3f1bab662569f2c32314f
Matthew C Wake <mcwake-dev@outlook.com>
2022-03-22 10:11:31 +0000

    Enabled cloud hosting for Redis and added Github workflow

--- db/db.js
+++ db/db.js
@@ -1,5 +1,12 @@
 const asyncRedis = require("async-redis");
-const client = asyncRedis.createClient();
+const client = process.env.NODE_ENV === "test-remote" || 
+               process.env.NODE_ENV === "production" ? 
+                asyncRedis.createClient({
+                  host: process.env.REDIS_HOST,
+                  port: process.env.REDIS_PORT,
+                  password: process.env.REDIS_PASSWORD
+                }) : 
+                asyncRedis.createClient();
 
 client.on("error", (err) => {
   console.log("Error " + err);
