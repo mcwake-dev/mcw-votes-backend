72f5187a9223301618ec4a909a4652cdbd55482c
Matthew C Wake <mcwake-dev@outlook.com>
2022-03-22 10:46:56 +0000

    Updated Redis connection to adapt to testing and production environments

--- db/db.js
+++ db/db.js
@@ -1,12 +1,23 @@
 const asyncRedis = require("async-redis");
-const client = process.env.NODE_ENV === "test-remote" || 
-               process.env.NODE_ENV === "production" ? 
-                asyncRedis.createClient({
-                  host: process.env.REDIS_HOST,
-                  port: process.env.REDIS_PORT,
-                  password: process.env.REDIS_PASSWORD
-                }) : 
-                asyncRedis.createClient();
+let client;
+
+switch(process.env.NODE_ENV) {
+  case "test-remote":
+    client = asyncRedis.createClient({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD
+    });
+    break;
+  case "production":
+    client = asyncRedis.createClient({
+      redis_url: REDISCLOUD_URL
+    });
+    break;
+  default: 
+    client = asyncRedis.createClient();
+    break;
+}
 
 client.on("error", (err) => {
   console.log("Error " + err);
